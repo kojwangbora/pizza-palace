596dd5802b71c43e1fd18fc509d73fba8e774cca
kojwangbora <kojwangbora254@gmail.com>
2022-01-17 19:44:29 +0300

    add js file

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         let pcrust=$("#crust option:selected").val();
         let ptopping=[];
         $.each($("input[name= 'toppings']:checked"), function(){
-            ptopping.push($(this).val());
+           ptopping.push($(this).val());
         });
         console.log(ptopping.join(", "));
 
@@ -81,6 +81,85 @@ $(document).ready(function(){
             $("#pizzacrust").html($("#crust option:selected").val());
             $("#pizztopping").html(ptopping.join(" ,"));
             $("#totals").html(totalprice);
+// pizza button
+        $("button.addPizza").click(function(){
+            let pname= $(".name option:selected").val();
+            let psize= $("#size option: selected").val();
+            let pcrust=$("#crust option: selected").val();
+            let ptopping=[];
+            $.each($("input[name='toppings']:checked"), function(){
+                ptopping.push($(this).val());
+            });
+            console.log(ptopping.join(","));
+            switch(psize){
+                case "0":
+                    price=0;
+
+                break
+                case "large":
+                    price= 1400;
+                    console.log(price);
+                break;
+                case "medium":
+                    price= 1000;
+                    console.log("pay"+price);
+                break;
+                case "small":
+                price=800;
+                console.log(price);
+              default:
+                console.log("error");
+
+            }
+            switch(pcrust){
+                case "0":
+                    crustprice=0;
+                break;
+                case "Stuffed" :
+                    crustprice= 200;
+                break;
+                case "Crispy":
+                    crustprice=100;
+                break;
+                case "Gluten-free":
+                    crustprice= 150;
+                break;
+              default:
+                    console.log("price?");
+            }
+                let topping_value=ptopping.length*150;
+                console.log("toppins value" + topping_value);
+                totalprice=price+crustprice+topping_value;
+                console.log(totalprice);
+
+                Checkouttotal=Checkouttotal+totalprice;
+                console.log(Checkouttotal);
+            var newOrder = new Getpizza(pname, psize, pcrust, ptopping, totalprice);
+            $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+            console.log(newOrder);    
+                
+        });
+        //Checkout
+        $("button.checkout").click(function(){
+            $("button#checkout").hide();
+            $("button.addPizza").hide();
+            $("button.deliver").slideRight(1200);
+            $("#addedprice").slideRight(1200);
+            console.log("total price amounts to Ksh."+ Checkouttotal);
+            $("#pizzatotal").append("your expense is Ksh."+ Checkouttotal);
+        });
+        //delivery
+        $("button.deliver").click(function(){
+            $(".pizzatable").hide();
+            $(".choose h2").hide();
+            $(".delivery").slideDown(1000);
+            $("#addedprice").hide();
+            $("button.deliver").hide();
+            $("#pizzatotal").hide();
+            let deliceryamount= checkoutTotal+150;
+            console.log("You will pay sh. "+deliceryamount+" on delivery");
+            $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
+          });
 
     });
 });
